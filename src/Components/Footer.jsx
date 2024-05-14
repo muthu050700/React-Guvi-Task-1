1f74c2092abcb017f90782708fa953b27725c037
muthu050700 <muthumuthu7699@gmail.com>
2024-05-14 20:20:34 +0530

    addes new data

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -94,7 +94,7 @@ function Footer() {
         className="text-center text-dark p-3"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
-        © 2020 Copyright:
+        © 2024 Copyright:
         <a className="text-dark" href="#">
           Muthukumaran.com
         </a>

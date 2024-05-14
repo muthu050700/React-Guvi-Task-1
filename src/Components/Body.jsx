9d7c9533b42bdeaa97e9da46cdea040dfd8faa7b
muthu050700 <muthumuthu7699@gmail.com>
2024-05-14 17:29:55 +0530

    added image in public

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,51 +1,12 @@
 import Carousel from "react-bootstrap/Carousel";
-
 function DarkVariantExample() {
   return (
-    // <Carousel data-bs-theme="dark" className="carousal-container">
-    //   <Carousel.Item>
-    //     <img
-    //       className="d-block w-100 img"
-    //       src="./src/assets/male-7275449_1920.jpg"
-    //       alt="First slide"
-    //     />
-    //     <Carousel.Caption >
-    //       <h5>First slide label</h5>
-    //       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    //   <Carousel.Item>
-    //     <img
-    //       className="d-block w-100 img"
-    //       src="./src/assets/woman-3040029_1920.jpg"
-    //       alt="Second slide"
-    //     />
-    //     <Carousel.Caption>
-    //       <h5>Second slide label</h5>
-    //       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    //   <Carousel.Item>
-    //     <img
-    //       className="d-block w-100 img"
-    //       src="./src/assets/shoes-4345656_1920.jpg"
-    //       alt="Third slide"
-    //     />
-    //     <Carousel.Caption>
-    //       <h5>Third slide label</h5>
-    //       <p>
-    //         Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-    //       </p>
-    //     </Carousel.Caption>
-    //   </Carousel.Item>
-    // </Carousel>
-
     <>
       <Carousel data-bs-theme="dark" className="container main-container">
         <Carousel.Item className="edit">
           <img
             className="d-block w-100"
-            src="./src/assets/bOvf94dPRxWu0u3QsPjF_tree.jpg"
+            src="./public/male-7275449_1920.jpg"
             alt="First slide"
           />
           <Carousel.Caption className="caption">
